332c8b6c0d4cee44a319a5c72a6c9d479732e4a4
afzsilva <afzsilva@gmail.com>
2022-04-08 15:55:17 -0300

    Implementa POST basico

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -2,8 +2,11 @@ package com.example.algamoneyapi.resource;
 
 import java.util.List;
 
+import org.apache.catalina.authenticator.SavedRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,11 @@ public class CategoriaResource {
 	public List<Categoria> listar() {
 		return repository.findAll();
 	}
+		
+	@PostMapping
+	public void criar(@RequestBody Categoria categoria) {
+		repository.save(categoria);
+	}
+	
 	
 }
